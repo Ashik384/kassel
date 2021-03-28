1c6d63224f7645fa6e7c08c35a96850b652d6783
Ashik <ashikulislam384@gmail.com>
2021-03-28 13:39:30 +0600

    adding tempaltes

---
+++ assets/js/active.js
@@ -0,0 +1,15 @@
+
+
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        $('.image_slider_selector').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+        });
+    });
+
+}(jQuery));
